9d9596ede2ab3db53c8f8c2aeb815f382866f378
Anderson Jorge <anderson.jorge33@gmail.com>
2022-01-11 20:32:08 -0300

    responsividade v1

--- js/main.js
+++ js/main.js
@@ -6,4 +6,29 @@ $('.slider-principal').slick({
   adaptiveHeight: true,
   autoplay: true,
   autoplaySpeed: 2000,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 425,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
 });
